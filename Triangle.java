7d940c62a543933ea5fc84bd88c2fe6cb7cac99d
Dinar <china.smorodina@yandex.ru>
2022-08-09 11:19:27 +0500

    Урок-4.-JUnit

---
+++ Triangle.java
@@ -0,0 +1,24 @@
+public class Triangle {
+
+    public static void main( String[] args )
+    {
+        double a;
+
+        a = triangleArea(3, 3, 3);
+        System.out.println("A triangle with sides 3,3,3 has an area of:" + a);
+
+        a = triangleArea(3, 4, 5);
+        System.out.println("A triangle with sides 3,4,5 has an area of:" + a);
+
+        a = triangleArea(9, 9, 9);
+        System.out.println("A triangle with sides 9,9,9 has an area of:" + a );
+
+    }
+
+    public static double triangleArea( int a, int b, int c )
+    {
+        double pp = (a + b + c) / 2.0;  // полупериметр
+        return Math.sqrt(pp * (pp - a) * (pp - b) * (pp - c)); // прямо по формуле Герона из вики
+    }
+
+}
